69ea2a8f815a5279fa8544739abb4676ab568799
sina96n <sina96n@gmail.com>
2021-12-17 18:02:30 -0800

    OOP - Project Update

--- 02.Object Oriented Programming - OOP/oop-project.py
+++ 02.Object Oriented Programming - OOP/oop-project.py
@@ -10,11 +10,11 @@ class Human():
     def rename(self, name):
         self.name = name
 
-
+    # defining a default representation form for the class
     def __str__(self) -> str:
         return self.name
 
-
+# class Player that inherits from class Human
 class Player(Human):
     def __init__(self, name) -> None:
         super().__init__(name)
@@ -29,18 +29,19 @@ class Player(Human):
     def team(self, t):
         self._team = t
 
+    # Adding a group of palyers at once
     @classmethod
-    def add_players_by_info(cls, name_lst, team):
+    def add_players_by_info(cls, names, team):
         players = list()
-        for i in range(len(name_lst)):
-            p = cls(name_lst[i])
+        for i in range(len(names)):
+            p = cls(names[i])
             p.team = team
 
             players.append(p)
 
         return players
 
-
+    # defining a default representation form for the class
     def __str__(self) -> str:
 
         if self.team ==  None:
@@ -49,19 +50,23 @@ class Player(Human):
             return f"{self.name} : {self.team} "
 
 
-
+# creating list of names
 names = [
     "Hassan", "Maziar" , "Akbar", "Nima", "Mehdi", "Farhad", "Mohammad", 
     "Khashayar", "Milad", "Mostafa", "Amin", "Saeed", "pouya", "pouria", 
     "Reza", "Ali", "Behzad", "Soheil", "Behruz", "Shahruz", "Saman", "Mohsen"
 ]
-teams = ["A","B"]
+teams = ["A","B"]       # desired teams
 
+# choosing random names to put in team A
+# putting all remainig names in team B
 random_names = random.sample(names, 11)
 remaining_names = [x for x in names if x not in random_names]
 
+# creating Player objects for each team
 team_a = Player.add_players_by_info(random_names, teams[0])
 team_b = Player.add_players_by_info(remaining_names, teams[1])
 
+# printing outputs
 print(*team_a, sep="\n")
 print(*team_b, sep="\n")
