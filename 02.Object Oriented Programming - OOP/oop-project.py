9f497df64e4f6af2798a14d9a5869e71f2e7b7ca
sina96n <sina96n@gmail.com>
2021-12-17 17:55:19 -0800

    OOP Project Update

--- 02.Object Oriented Programming - OOP/oop-project.py
+++ 02.Object Oriented Programming - OOP/oop-project.py
@@ -1,18 +1,52 @@
 import random
 
 class Human():
+    count = 0
     def __init__(self, name) -> None:
         self.name = name
+
+        Human.count += 1 
+
+    def rename(self, name):
+        self.name = name
+
 
     def __str__(self) -> str:
         return self.name
 
 
 class Player(Human):
-    def __init__(self, name, team) -> None:
+    def __init__(self, name) -> None:
         super().__init__(name)
 
-        self.team = team
+        self._team = None
+
+    @property
+    def team(self):
+        return self._team
+
+    @team.setter
+    def team(self, t):
+        self._team = t
+
+    @classmethod
+    def add_players_by_info(cls, name_lst, team):
+        players = list()
+        for i in range(len(name_lst)):
+            p = cls(name_lst[i])
+            p.team = team
+
+            players.append(p)
+
+        return players
+
+
+    def __str__(self) -> str:
+
+        if self.team ==  None:
+            return f"{self.name} : not a Team member."
+        else:
+            return f"{self.name} : {self.team} "
 
 
 
@@ -23,7 +57,11 @@ names = [
 ]
 teams = ["A","B"]
 
+random_names = random.sample(names, 11)
+remaining_names = [x for x in names if x not in random_names]
 
+team_a = Player.add_players_by_info(random_names, teams[0])
+team_b = Player.add_players_by_info(remaining_names, teams[1])
 
-# a = Player("Sina")
-# print(a)
+print(*team_a, sep="\n")
+print(*team_b, sep="\n")
