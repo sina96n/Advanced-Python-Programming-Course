7861628307e364380d7378c836d6da4e8c417cc9
sina96n <sina96n@gmail.com>
2021-12-19 11:44:36 -0800

    Email Validator

---
+++ 04.Web Scraping with Python/Email-Validator.py
@@ -0,0 +1,27 @@
+import re 
+
+# regular expression for email
+email_regex = r"[\w+\d]+@\w+.\w+"
+
+# email validator function
+# matches the written regex with given email address
+def email_validator(email):
+    if re.fullmatch(email_regex,email):
+        return True
+    else:
+        return False
+
+# taking user email address
+email = input()
+flag = email_validator(email)
+
+# output 
+if flag:
+    print("OK")
+else:
+    print("WRONG")
+
+
+
+# By Sina Kazemi
+# https://github.com/sina96n
---
+++ 04.Web Scraping with Python/test.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get()
+
+soup = BeautifulSoup()
